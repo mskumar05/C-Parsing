b31bf61c81831f9e10e04ea5cbd404c6356019ea
mskumar05 <msk05@myyahoo.com>
2025-04-06 17:06:29 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿/*namespace Perse.code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Student Id:");
+        int sid = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Student Name:");
+        string sname = Console.ReadLine();
+        Console.WriteLine("Enter Student Location:");
+        string sloc = (Console.ReadLine());
+        Console.WriteLine("Student Id is:" + sid);
+        Console.WriteLine("Student Name is:" + sname);
+        Console.WriteLine("Student Location is:" + sloc);
+        Console.ReadLine();
+    }
+} 
+
+//Total marks of a student m1.m2,m3.
+
+namespace Perse.code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter First Subject Marks:");
+        double m1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Subject Marks:");
+        double m2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Third Subject Marks:");
+        double m3 = double.Parse(Console.ReadLine());
+        double totmarks = m1 + m2 + m3;
+        Console.WriteLine("Your Total Marks are:" + totmarks);
+        Console.ReadLine();
+
+    }
+
+}
+
+//Total marks and average marks of 3student.
+
+namespace Perse.code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter First Subject Marks:");
+        double m1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Subject Marks:");
+        double m2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Third Subject Marks:");
+        double m3 = double.Parse(Console.ReadLine());
+        double totmarks = m1 + m2 + m3;
+        double avgmarks = totmarks / 3;
+        Console.WriteLine("Your Total Marks are:" + totmarks);
+        Console.WriteLine("Your Avg Marks are:" + avgmarks);
+        Console.ReadLine();
+    }
+
+}
+
+
+//Total marks and average marks of 12student.
+
+namespace Perse.code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter First Subject Marks:");
+        double m1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Subject Marks:");
+        double m2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Third Subject Marks:");
+        double m3 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Fourth Subjact Marks:");
+        double m4 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Fifth Subject Marks");
+        double m5 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Sixth Subject Marks");
+        double m6 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Seventh Subject Marks:");
+        double m7 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Eighth Subject Marks:");
+        double m8 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Ninth Subject Marks:");
+        double m9 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Tenth Subjact Marks:");
+        double m10 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Eleventh Subject Marks");
+        double m11 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Tweleth Subject Marks");
+        double m12 = double.Parse(Console.ReadLine());
+        double totmarks = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12;
+        double avgmarks = totmarks / 12;
+        Console.WriteLine("Your Total Marks are:" + totmarks);
+        Console.WriteLine("Your Avg Marks are:" + avgmarks);
+        Console.ReadLine();
+    }
+
+}
+
+//Accept 4products cost and display the total bill after discount:
+
+namespace Perse.code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter First Product Cost:");
+        double p1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Product Cost:");
+        double p2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Third Product Cost:");
+        double p3 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Fourth Product Cost:");
+        double p4 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Discount:");
+        double disc = double.Parse(Console.ReadLine());
+        double totbill = p1 + p2 + p3 + p4;
+        disc = (totbill * disc) / 100;
+        totbill = totbill - disc;
+        Console.WriteLine("Your Total Bill is:" + totbill);
+        Console.ReadLine();
+    }
+}*/
+
